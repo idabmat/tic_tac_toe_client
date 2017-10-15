476f1305e13c1015afed871f1ac6c3168da22f34
Igor de Alcantara Barroso <idabmat@gmail.com>
2017-10-15 04:58:33 +0100

    Hook up client to phoenix channels

--- components/App/index.js
+++ components/App/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Socket } from 'phoenix'
 import Grid from '../Grid'
 
 export default class App extends React.Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
-      currentPlayer: null,
       board: [
         [null, null, null],
         [null, null, null],
@@ -14,13 +14,44 @@ export default class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.socket = new Socket('ws://localhost:4000/socket')
+    this.socket.connect()
+    this.channel = this.socket.channel('game:new', {})
+  }
+
+  startGame() {
+    this.channel.join()
+    this.channel.on('game_state', (gameState) => this.updateState(gameState))
+    this.channel.push('get_state')
+  }
+
+  newGame() {
+    this.channel.push('new_game')
+  }
+
+  updateState(gameState) {
+    this.setState(gameState)
+    if (gameState.current_player === 'computer') {
+      this.computerMove()
+    }
+  }
+
+  computerMove() {
+    this.channel.push('computer_move')
+  }
+
   handleCellClicked(position) {
-    console.log(position)
+    this.channel.push('player_move', position)
   }
 
   render() {
     return(
-      <Grid board={ this.state.board } cellClicked={ this.handleCellClicked.bind(this) }/>
+      <div>
+        <button onClick={this.startGame.bind(this)}>Start</button>
+        <button onClick={this.newGame.bind(this)}>New game</button>
+        <Grid board={ this.state.board } cellClicked={ this.handleCellClicked.bind(this) }/>
+      </div>
     )
   }
 }
--- components/App/index.test.js
+++ components/App/index.test.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../../config/setupTests'
 import { shallow } from 'enzyme'
+import sinon from 'sinon'
 import App from '.'
 
+const NOOP = (e) => e
+
 describe('App', () => {
-  let wrapper
+  let wrapper, mock
 
   beforeEach(() => {
     wrapper = shallow(<App />)
@@ -15,12 +18,60 @@ describe('App', () => {
       const emptyBoard = [
         [null, null, null],
         [null, null, null],
-        [null, null, null],
+        [null, null, null]
       ]
       expect(wrapper.state().board).toEqual(emptyBoard)
     })
-    it('loads without a currentPlayer', () => {
-      expect(wrapper.state().currentPlayer).toBe(null)
+  })
+
+  describe('WebSocket channel', () => {
+    let channelMock
+
+    beforeEach(() => {
+      channelMock = sinon.mock(wrapper.instance().channel)
+    })
+
+    afterEach(() => {
+      channelMock.restore()
+    })
+
+    it('newGame pushes new_game message to the socket', () => {
+      channelMock.expects('push').withArgs('new_game')
+      wrapper.instance().newGame()
+      channelMock.verify
+    })
+
+    it('computerMove pushes computer_move message to the socket', () => {
+      channelMock.expects('push').withArgs('computer_move')
+      wrapper.instance().computerMove()
+      channelMock.verify
+    })
+
+    it('handleCellClicked pushes player_move message to the socket', () => {
+      channelMock.expects('push').withArgs('player_move', 3)
+      wrapper.instance().handleCellClicked(3)
+      channelMock.verify
+    })
+  })
+
+  describe('updateState', () => {
+    it('updates the component state', () => {
+      const gameState = {
+        board: [
+          [null, null     , null],
+          [null, 'player1', null],
+          [null, null     , null]
+        ]
+      }
+      wrapper.instance().updateState(gameState)
+      expect(wrapper.state()).toEqual(gameState)
+    })
+
+    it('calls computer move if it is the computer\'s turn', () => {
+      const gameState = { current_player: 'computer' }
+      const computerMoveStub = sinon.stub(App.prototype, 'computerMove')
+      wrapper.instance().updateState(gameState)
+      expect(computerMoveStub.calledOnce).toEqual(true)
     })
   })
 })
