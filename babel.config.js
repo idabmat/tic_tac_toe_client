325e901b5dc7275184d4e1c782a39e04fb2fec68
Igor de Alcantara Barroso <idabmat@gmail.com>
2021-02-06 15:03:10 +0000

    Clean up and update dependencies

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  env: {
+    production: {
+      plugins: ['styled-jsx/babel']
+    },
+    development: {
+      plugins: ['styled-jsx/babel']
+    },
+    test: {
+      plugins: ['styled-jsx/babel-test']
+    },
+  }
+}
